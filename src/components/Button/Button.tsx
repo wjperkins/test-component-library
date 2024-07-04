299bba579ed0e4ce79c0b22cc17f7082625dfbcb
Will Perkins <wjperkins@users.noreply.github.com>
2024-07-04 13:08:48 +0100

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export function Button(
   const { className = "", ...rest } = props;
   return (
     <button
-      className={`${className} rounded-md bg-blue-600 p-2 px-3 text-white hover:bg-blue-500 dark:bg-blue-300 dark:text-gray-900 dark:hover:bg-blue-100`}
+      className={`${className} rounded-md bg-blue-700 p-2 px-3 text-white hover:bg-blue-500 dark:bg-blue-300 dark:text-gray-900 dark:hover:bg-blue-100`}
       {...rest}
     />
   );
